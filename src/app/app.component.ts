0c266ccd23e7035c905499f062dfa791152282a3
wes35 <lachand_michael@hotmail.com>
2022-02-21 16:32:37 +0100

    porjet terminé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'PhotoPartage';
+export class AppComponent implements OnInit{
+
+
+
+  ngOnInit() {
+
+  }
+
+
 }
